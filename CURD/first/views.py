010f0f9056475939971fa46905e6700eff073afb
Sujal17-git <sujalthakor.education@gmail.com>
2025-05-16 21:55:03 +0530

    Sum of Two Number

--- CURD/first/views.py
+++ CURD/first/views.py
@@ -4,4 +4,12 @@ from django.http import HttpResponse
 def hello(request):
     return render(request,'home.html')
 
+def add(request):
+
+    num1 = request.GET['num1']
+    num2 = request.GET['num2']
+    result = int(num1) + int(num2)
+    return render(request,'result.html',{'result' : result})
+    
+
 # Create your views here.
